ffafb5f99059d09e146fbdb0e8eb764708f0e504
Ben Li <benbli@gmail.com>
2016-04-27 14:21:33 -0400

    deliverable mvp

--- app/models/supermodel.js
+++ app/models/supermodel.js
@@ -7,21 +7,23 @@ var SupermodelSchema = new Schema({
     picture: { type: Schema.Types.Mixed, required: true},
     morePictures: Schema.Types.Mixed,
     height: { type: String },
-    bust: { type: String },
-    waist: { type: String },
-    hips: { type: String },
-    shoes: { type: String },
-    eyes: { type: String },
-    hair: { type: String },
     createdAt: { type: Date, default: Date.now}
+
+    // **** Future implementation ****
+    // bust: { type: String },
+    // waist: { type: String },
+    // hips: { type: String },
+    // shoes: { type: String },
+    // eyes: { type: String },
+    // hair: { type: String },
 });
 
-// SupermodelSchema.pre('save', function(next) {
-//   now = new Date();
-//   if (!this.createdAt) {
-//     this.createdAt = now;
-//   }
-//   next();
-// });
+SupermodelSchema.pre('save', function(next) {
+  now = new Date();
+  if (!this.createdAt) {
+    this.createdAt = now;
+  }
+  next();
+});
 
 module.exports = mongoose.model('Supermodel', SupermodelSchema);
